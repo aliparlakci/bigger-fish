fb156a02b86be7f8562ebea3984ee00954820a23
umuterenornek <ueren123@gmail.com>
2022-12-19 23:21:55 +0300

    Start work on supporting multiple video player testing

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import pickle
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(description='Automate the collection of video player based CPU taces.')
+parser = argparse.ArgumentParser(description='Automate the collection of video player based CPU traces.')
 parser.add_argument("--trace_len", type=int, default=1, help="The trace length for the recordings in seconds.")
 parser.add_argument("--num_runs", type=int, default=5, help="The number of runs for each video file.")
 parser.add_argument("--out_dir", type=str, default="", help="The output location.")
+parser.add_argument("--var", choices=["codec", "player"], default="player", help="The variable that will be changed. \
+        Choosing the codec will record traces for each codec and vice versa.")
 opts = parser.parse_args()
 
 NUM_OF_RUNS = opts.num_runs
@@ -23,7 +25,7 @@ def run(file_path, trace_length):
     with TraceCollector(trace_length=trace_length) as collector:
         player = VideoPlayer()
 
-        player_thread = threading.Thread(target = lambda: player.play(file_path, trace_length))
+        player_thread = threading.Thread(target=lambda: player.play(file_path, trace_length))
         player_thread.start()
 
         trace = collector.collect_traces()
@@ -31,9 +33,21 @@ def run(file_path, trace_length):
         player_thread.join()
 
 
-for file in ["sample.3gp", "sample.flv", "sample.mkv", "sample.mp4"]:
-    for _ in range(NUM_OF_RUNS):
-        run(file, TRACE_LENGTH)
+def main():
+    if opts.control == "player":
+        for player in VideoPlayer.players:
+            file = "sample.mp4"
+            for _ in range(NUM_OF_RUNS):
+                run(file, TRACE_LENGTH)
+    elif opts.control == "codec":
+        for file_path in VIDEO_FILES:
+            for player in SupportedPlayers:
+                for i in range(NUM_OF_RUNS):
+                    run(file_path, TRACE_LENGTH)
 
-with open(fr"{OUT_DIR}/traces_{NUM_OF_RUNS}_runs_{TRACE_LENGTH}_sec_{sys.platform}_OS.pkl", "wb") as file:
-    pickle.dump(traces, file)
+    with open(OUT_DIR, "wb") as f:
+        pickle.dump(traces, f)
+
+
+if __name__ == "__main__":
+    main()
--- video_player.py
+++ video_player.py
@@ -5,15 +5,18 @@ import sys
 from enum import Enum
 
 
-class SUPPORTED_PLAYERS(Enum):
+class SupportedPlayers(Enum):
     WINDOWS_MEDIA_PLAYER = 1
-    MPC = 2
+    MPLAYER = 2
     VLC = 3
 
 
 class VideoPlayer:
-    def __init__(self, player=SUPPORTED_PLAYERS.VLC):
-        pass
+    players = None
+    player = None
+
+    def __init__(self, players=SupportedPlayers):
+        self.players = players
 
     def play(self, file_path, length):
         player_name = {"linux": "vlc", "win32": "vlc.exe", "darwin": "/Applications/VLC.app/Contents/MacOS/VLC"}[
